58e5d7b20639403f84c4040c6752a4c56fbb8112
betelhem16 <betelhem.kassaye@aastustudent.edu.et>
2025-10-24 17:09:05 +0300

    Add /api/users/me/ endpoint for user self-profile access

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, UserListView
+from .views import RegisterView, UserListView, UserDetailView
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('list/', UserListView.as_view(), name='user_list'),
+    path('api/users/me/', UserDetailView.as_view(), name='user-detail'),
+
 ]
